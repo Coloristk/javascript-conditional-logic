a83bb85ff9895c50c2b836427487612623f8cc67
Kong Thiraphat <thiraphat.cto@gmail.com>
2024-10-03 14:13:02 +0700

    feat: add answer to exercise

--- ex-2.js
+++ ex-2.js
@@ -2,3 +2,17 @@
 let lightBulbStatus = "On";
 
 // Start coding here.
+lightBulbStatus = "Broken"
+
+if (lightBulbStatus === "On") {
+    lightBulbStatus = "Light bulb is On."
+}else if (lightBulbStatus === "Off") {
+    lightBulbStatus = "Light bulb is Off."
+}else if (lightBulbStatus === "Broken"){
+    lightBulbStatus = "Light bulb is Broken."
+}else {
+    lightBulbStatus = "Please choose the correct input (On/Off/Broken)"
+}
+
+
+console.log(lightBulbStatus)
